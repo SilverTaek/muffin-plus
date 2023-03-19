1e931d2272a4fedbd41516111dbbbe6b2b8e59eb
DESKTOP-07B2546\rudus <rudus1012@naver.com>
2023-03-19 15:17:28 +0900

    feat: project initial commit

---
+++ src/main/java/com/muffin/plus/muffinplus/MuffinPlusApplication.java
@@ -0,0 +1,13 @@
+package com.muffin.plus.muffinplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuffinPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuffinPlusApplication.class, args);
+	}
+
+}
