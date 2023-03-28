56caf5fc26d7f0eaee84b546b66e371dbd50c4dc
DESKTOP-07B2546\rudus <rudus1012@naver.com>
2023-03-28 23:30:00 +0900

    feat: Entity 생성

--- src/main/java/com/muffin/plus/muffinplus/domain/Reservation.java
+++ src/main/java/com/muffin/plus/muffinplus/domain/Reservation.java
@@ -1,4 +1,35 @@
 package com.muffin.plus.muffinplus.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int status;
+
+    private int chgerType;
+
+    private int zcode;
+
+    private int zscode;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateUpdated;
+
+    private LocalDateTime dateDeleted;
 }
--- src/main/java/com/muffin/plus/muffinplus/domain/User.java
+++ src/main/java/com/muffin/plus/muffinplus/domain/User.java
@@ -1,4 +1,32 @@
 package com.muffin.plus.muffinplus.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private int type;
+
+    private LocalDateTime dateCreated;
+
+    private LocalDateTime dateUpdated;
+
+    private LocalDateTime dateDeleted;
 }
